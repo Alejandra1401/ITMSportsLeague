92874dd127ced6bbe43f372f8307d475f25ff0c7
Alejandra1401 <alejandra.reyes.dias17@gmail.com>
2026-03-16 20:24:42 -0500

    Phase2-first part New Player Entity creasted

--- SportsLeague.DataAccess/Context/LeagueDbContext.cs
+++ SportsLeague.DataAccess/Context/LeagueDbContext.cs
@@ -11,8 +11,9 @@ namespace SportsLeague.DataAccess.Context
         }
 
         public DbSet<Team> Teams => Set<Team>();
+        public DbSet<Player> Players => Set<Player>();
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)//METODO PARA MODELAR LAS ENTIDADES SEGUN SUS PROPIEDADES 
         {
             base.OnModelCreating(modelBuilder);
 
@@ -34,6 +35,37 @@ namespace SportsLeague.DataAccess.Context
                 entity.Property(t => t.UpdatedAt)
                       .IsRequired(false);
                 entity.HasIndex(t => t.Name)
+                      .IsUnique();
+            });
+            // ── Player Configuration ──
+            modelBuilder.Entity<Player>(entity =>
+            {
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.FirstName)
+                      .IsRequired()
+                      .HasMaxLength(80);
+                entity.Property(p => p.LastName)
+                      .IsRequired()
+                      .HasMaxLength(80);
+                entity.Property(p => p.BirthDate)
+                      .IsRequired();
+                entity.Property(p => p.Number)
+                      .IsRequired();
+                entity.Property(p => p.Position)
+                      .IsRequired();
+                entity.Property(p => p.CreatedAt)
+                      .IsRequired();
+                entity.Property(p => p.UpdatedAt)
+                      .IsRequired(false);
+
+                // Relación 1:N con Team
+                entity.HasOne(p => p.Team)
+                      .WithMany(t => t.Players)
+                      .HasForeignKey(p => p.TeamId)
+                      .OnDelete(DeleteBehavior.Cascade);//el borrador en cascada me elimina a todos los jugadores que esten asociados a un equipo cuando se borra el equipo
+
+                // Índice único compuesto: número de camiseta único por equipo
+                entity.HasIndex(p => new { p.TeamId, p.Number })//Por cada equipo solo puede haber un jugador con un número de camiseta específico, evitando duplicados dentro del mismo equipo
                       .IsUnique();
             });
         }
---
+++ SportsLeague.Domain/Enums/PlayerPosition.cs
@@ -0,0 +1,10 @@
+﻿namespace SportsLeague.Domain.Enums
+{
+    public enum PlayerPosition//Fijo los valores de la posiciones de los jugadores para evitar problemas de compatibilidad en el futuro
+    {
+        Goalkeeper = 0,
+        Defender = 1,
+        Midfielder = 2,
+        Forward = 3
+    }
+}
