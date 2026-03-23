6c963464db5b09877443d724cabbeab0411f5ed5
Alejandra1401 <alejandra.reyes.dias17@gmail.com>
2026-03-23 10:37:49 -0500

    Phase 2 - part 2 and PHASE 3- part 1 -- New 3 entities created: Referee, Tournament and tournamentTeam *Pending: part 2 Phate3 : DTOs,Mappers and Controllers

---
+++ SportsLeague.Domain/Entities/TournamentTeam.cs
@@ -0,0 +1,14 @@
+﻿namespace SportsLeague.Domain.Entities
+{
+    public class TournamentTeam : AuditBase
+    {
+        public int TournamentId { get; set; }//FK TABLA DERECHA
+        public int TeamId { get; set; }//FK TABLA IZQUIERDA
+        public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation Properties
+        public Tournament Tournament { get; set; } = null!;
+        public Team Team { get; set; } = null!;
+    }
+
+}
---
+++ SportsLeague.Domain/Interfaces/Repositories/ITournamentTeamRepository.cs
@@ -0,0 +1,11 @@
+﻿using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.Domain.Interfaces.Repositories
+{
+    public interface ITournamentTeamRepository : IGenericRepository<TournamentTeam>
+    {
+        Task<TournamentTeam?> GetByTournamentAndTeamAsync(int tournamentId, int teamId);// Método para obtener la relación entre un torneo y un equipo específico
+        Task<IEnumerable<TournamentTeam>> GetByTournamentAsync(int tournamentId);//Metodo para obtener todos los equipos registrados en un torneo específico
+    }
+
+}
